f652b9b21650b1b5031d379a0435ffef23d2e08f
bbfodor <fodor.balint97@gmail.com>
2023-12-06 11:39:02 +0100

    Add project files.

---
+++ AdventOfCode23/ISolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode23
+{
+    public interface ISolution<TSolution1, TSolution2>
+    {
+
+        public TSolution1 GetSolution1();
+
+        public TSolution2 GetSolution2();
+    }
+}
---
+++ AdventOfCode23/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode23;
+
+Console.WriteLine("Day 1");
+Console.WriteLine("==========");
+Day1 day1 = new("day1.file");
+int day1Solution1 = day1.GetSolution1();
+Console.WriteLine($"Solution 1: {day1Solution1}");
+
+Console.WriteLine("\n\nSolution succesful.");
